7f8e9688b7e8ec57cb1505852c2b6c418508214d
J3m3 <y.j3ms.n@gmail.com>
2025-05-08 15:18:35 +0900

    Improve build script
    - add user-friendly help messages
    - only show compile commands
    - support `--with` option

--- build.sh
+++ build.sh
@@ -1,17 +1,39 @@
 #!/bin/sh
 
-set -xe
+# Obviously, only works on POSIX-compliant systems.
 
-if command -v gcc >/dev/null 2>&1; then
-    CC=gcc
-elif command -v clang >/dev/null 2>&1; then
-    CC=clang
-else
-    echo "MSVC user? I'm pretty sure you know what to do next." >&2
-    exit 1
+set -e
+
+CC=""
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --with|-with)
+            shift
+            CC="$1"
+            ;;
+        *)
+            echo "Usage: $0 [--with|-with <compiler>]" >&2
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$CC" ]; then
+    if command -v gcc >/dev/null 2>&1; then
+        CC=gcc
+    elif command -v clang >/dev/null 2>&1; then
+        CC=clang
+    else
+        echo "ERROR: no C comiler found; use --with=<compiler>" >&2
+        exit 1
+    fi
 fi
 
 src=genericc.c
 
-$CC -E $src > "expanded_$src"
+set -x
+
 $CC -o "${src%.c}" -g $src
+$CC -E $src > "expanded_$src" 2>/dev/null
